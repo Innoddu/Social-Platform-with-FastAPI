f2d82f19321bd792edf9136c09ff144c5685b455
innoddu <ciui9494@gmail.com>
2024-05-26 11:20:27 -0700

    staging changes before running filter-branch

--- venv/alembic/env.py
+++ venv/alembic/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from models.feed import Feed
+from models.user import User
 
 
 # this is the Alembic Config object, which provides
@@ -20,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Feed.metadata
+target_metadata = User.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
